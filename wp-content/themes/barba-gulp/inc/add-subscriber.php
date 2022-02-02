55ab97f1d06fea20d746a9ac5bb108d88bde881e
jura <yury.andreichuk@follow.vision>
2022-02-02 15:42:24 +0100

    change php subscribe function

--- wp-content/themes/barba-gulp/inc/add-subscriber.php
+++ wp-content/themes/barba-gulp/inc/add-subscriber.php
@@ -1,29 +1,21 @@
-<?php 
+<?php
 
 function add_subscriber() {
   $project_name = trim($_POST["project_name"]);
   $admin_email  = trim($_POST["admin_email"]);
   $form_subject = trim($_POST["form_subject"]);
-  $user_name = trim($_POST["name"]);
-  $user_mail = trim($_POST["email"]);
-
-  $subscriber =  array(
-      'apel_name' => $user_name,
-      'apel_email' => $user_mail
-  );
-
-  add_row('apel', $subscriber, 'options');
-
-  $message = "<p>Nowy sygnatariusz na stronie - <b>'.$project_name.'</b></p>";
   
+
+  
+  $message .= 'Nowy sygnatariusz na Stronie: '.$project_name;  
   function adopt($text) {
     return '=?UTF-8?B?'.Base64_encode($text).'?=';
   }
   
   $headers = "MIME-Version: 1.0" . PHP_EOL .
   "Content-Type: text/html; charset=utf-8" . PHP_EOL .
-  'From: '.adopt($project_name).' <yury.andreichuk@follow.vision>' . PHP_EOL .
-  'Reply-To: yury.andreichuk@follow.vision' . PHP_EOL;
+  'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
+  'Reply-To: '.$admin_email.'' . PHP_EOL;
   
   mail($admin_email, adopt($form_subject), $message, $headers );
 }
