8432cd6b8e2e63c231a6e70ae68d40a43fa62064
jura <yury.andreichuk@follow.vision>
2022-02-02 15:54:55 +0100

    test

--- wp-content/themes/barba-gulp/inc/add-subscriber.php
+++ wp-content/themes/barba-gulp/inc/add-subscriber.php
@@ -4,18 +4,18 @@ function add_subscriber() {
   $project_name = trim($_POST["project_name"]);
   $admin_email  = trim($_POST["admin_email"]);
   $form_subject = trim($_POST["form_subject"]);
-  
+  $from_mail = 'michal.pietrzok@follow.vision';
 
-  
-  $message .= 'Nowy sygnatariusz na Stronie: '.$project_name;  
+  $message .= 'Nowy sygnatariusz na Stronie PR to nie propaganda';  
+
   function adopt($text) {
     return '=?UTF-8?B?'.Base64_encode($text).'?=';
   }
   
   $headers = "MIME-Version: 1.0" . PHP_EOL .
   "Content-Type: text/html; charset=utf-8" . PHP_EOL .
-  'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
-  'Reply-To: '.$admin_email.'' . PHP_EOL;
+  'From: '.adopt($project_name).' <'.$from_mail.'>' . PHP_EOL .
+  'Reply-To: '.$from_mail.'' . PHP_EOL;
   
   mail($admin_email, adopt($form_subject), $message, $headers );
 }
