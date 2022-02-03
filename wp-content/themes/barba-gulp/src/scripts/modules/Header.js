bbc753dbe01ab70a5ed15b43c915410136b7b7f6
wikxn <wiktoriaxniezborala@gmail.com>
2022-02-03 11:51:14 +0100

    form-validation

--- wp-content/themes/barba-gulp/src/scripts/modules/Header.js
+++ wp-content/themes/barba-gulp/src/scripts/modules/Header.js
@@ -6,21 +6,24 @@ class Header {
     this.options = document.querySelectorAll('.header__option');
 
     this.burger.addEventListener('click', () => {
-      if (this.mobileMenu.style.display === 'none') {
-        this.mobileMenu.style.display = 'block'
-        this.burger.classList.add('burger--open')
-      } else {
-        this.mobileMenu.style.display = 'none'
-        this.burger.classList.remove('burger--open')
-      }
+      this.mobileMenu.style.display === 'none' ? this.openMenu() : this.closeMenu();
     })
 
     this.options.forEach((option) => {
       option.addEventListener('click', () => {
-        this.mobileMenu.style.display = 'none';
-        this.burger.classList.remove('burger--open');
+        this.closeMenu();
       })
     })
+  }
+
+  openMenu() {
+    this.mobileMenu.style.display = 'block';
+    this.burger.classList.add('burger--open');
+  }
+
+  closeMenu() {
+    this.mobileMenu.style.display = 'none';
+    this.burger.classList.remove('burger--open');
   }
 }
 
--- wp-content/themes/barba-gulp/template-parts/popup.php
+++ wp-content/themes/barba-gulp/template-parts/popup.php
@@ -9,37 +9,35 @@
           </div>
           <h2 class="page-popup__title">Podpisz apel</h2>
           <p class="page-popup__information">Podpisując nasz apel sprzeciwiasz się nadużyciom, które negatywnie wpływają
-            na
-            wizerunek branży PR. Bo PR to nie propaganda! </p>
-          <form class="page-popup__form">
-            <input type="text" placeholder="Imię i nazwisko" class="page-popup__input">
-            <input type="email" placeholder="E-mail" class="page-popup__input">
+            na wizerunek branży PR. Bo PR to nie propaganda! </p>
+          <form id="form" class="page-popup__form">
+            <input data-popup="name" type="text" placeholder="Imię i nazwisko" class="page-popup__input">
+            <input data-popup="email" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+              placeholder="E-mail" class="page-popup__input">
             <p class="page-popup__subtitle">Wymagane zgody</p>
             <div class="page-popup__check-wrap d-flex">
-              <input type="checkbox" name="privacy" id="" class="page-popup__check">
+              <input data-popup="check" type="checkbox" name="check" id="" class="page-popup__check">
               <span class="page-popup__checkmark"></span>
               <label for="privacy" class="page-popup__label">Zapoznałem się i akceptuję Politykę Prywatności
                 ZFPR*</label>
             </div>
             <div class="page-popup__check-wrap d-flex">
-              <input type="checkbox" name="data" id="" class="page-popup__check">
+              <input data-popup="check" type="checkbox" name="check" id="" class="page-popup__check">
               <span class="page-popup__checkmark"></span>
               <label for="data" class="page-popup__label">Wyrażam zgodę na przetwarzanie podanych przeze mnie w
-                formularzu
-                danych osobowych przez administratora danych osobowych, którym jest Związek Firm Public Relations z
-                siedzibą w Warszawie 00-732, przy ul. Czerskiej 8/10 („ZFPR”) w celu podpisania „Apelu” *</label>
+                formularzu danych osobowych przez administratora danych osobowych, którym jest Związek Firm Public
+                Relations z siedzibą w Warszawie 00-732, przy ul. Czerskiej 8/10 („ZFPR”) w celu podpisania „Apelu” *</label>
             </div>
             <div class="page-popup__check-wrap d-flex">
-              <input type="checkbox" name="public" id="" class="page-popup__check">
+              <input data-popup="check" type="checkbox" name="check" id="" class="page-popup__check">
               <span class="page-popup__checkmark"></span>
               <label for="public" class="page-popup__label">Wyrażam zgodę na publikację przez Związek Firm Public
-                Relations
-                mojego imienia i nazwiska na stronie internetowej www.prtoniepropaganda.pl (zgoda jest dobrowolna, ale
+                Relations mojego imienia i nazwiska na stronie internetowej www.prtoniepropaganda.pl (zgoda jest dobrowolna, ale
                 wymagana do podpisania „Apelu”)* <br> <br>
                 *Zgody obowiązkowe</label>
             </div>
             <div class="page-popup__buton-wrap d-flex justify-content-center">
-              <button class="page-popup__button">Podpisuję</button>
+              <button data-popup="sign" class="page-popup__button">Podpisuję</button>
             </div>
           </form>
         </div>
