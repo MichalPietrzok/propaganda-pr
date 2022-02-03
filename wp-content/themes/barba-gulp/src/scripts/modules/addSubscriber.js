69b429f0451dae8a4b17d5f1f3a62178a7789559
jura <yury.andreichuk@follow.vision>
2022-02-03 16:09:03 +0100

    add TY popup to apel

--- wp-content/themes/barba-gulp/src/scripts/modules/addSubscriber.js
+++ wp-content/themes/barba-gulp/src/scripts/modules/addSubscriber.js
@@ -1,4 +1,6 @@
 const addSubscriber = (targetForm) => {
+  const thanks = document.querySelector('[data-popup="thanks"]')
+  const mainWrapper = document.querySelector('.page-popup')
   const request = new XMLHttpRequest();
   request.open('POST', ajaxurl, true);
   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
@@ -6,6 +8,8 @@ const addSubscriber = (targetForm) => {
   request.onreadystatechange = function () {
     if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
       setTimeout(() => {
+        mainWrapper.style.display = 'none';
+        thanks.style.display = 'block';
         targetForm.reset()
       }, 500)
     }
