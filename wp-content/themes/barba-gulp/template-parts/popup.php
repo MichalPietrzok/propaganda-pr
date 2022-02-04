4e2cc391f66c1f500021a6c589ba45eba335c4b0
j.andreiczuk <andreiczuk.tutanota.com>
2022-02-04 18:46:46 +0100

    finish popup

--- wp-content/themes/barba-gulp/template-parts/popup.php
+++ wp-content/themes/barba-gulp/template-parts/popup.php
@@ -18,17 +18,17 @@
         <input type="email" data-popup="email" placeholder="E-mail" name="email" class="page-popup__input">
         <p class="page-popup__subtitle">Wymagane zgody</p>
         <div class="page-popup__check-wrap d-flex">
-          <input type="checkbox" name="privacy" id="" class="page-popup__check">
+          <input id="privacy" type="checkbox" name="privacy" id="" class="page-popup__check">
           <span class="page-popup__checkmark"></span>
           <label for="privacy" class="page-popup__label">Zapoznałem się i akceptuję Politykę Prywatności ZFPR*</label>
         </div>
         <div class="page-popup__check-wrap d-flex">
-          <input type="checkbox" name="data" id="" class="page-popup__check">
+          <input id="data" type="checkbox" name="data" id="" class="page-popup__check">
           <span class="page-popup__checkmark"></span>
           <label for="data" class="page-popup__label">Wyrażam zgodę na przetwarzanie podanych przeze mnie w formularzu danych osobowych przez administratora danych osobowych, którym jest Związek Firm Public Relations z siedzibą w Warszawie 00-732, przy ul. Czerskiej 8/10 („ZFPR”) w celu podpisania „Apelu” *</label>
         </div>
         <div class="page-popup__check-wrap d-flex">
-          <input type="checkbox" name="public" id="" class="page-popup__check">
+          <input id="public" type="checkbox" name="public" id="" class="page-popup__check">
           <span class="page-popup__checkmark"></span>
           <label for="public" class="page-popup__label">Wyrażam zgodę na publikację przez Związek Firm Public Relations mojego imienia i nazwiska na stronie internetowej www.prtoniepropaganda.pl (zgoda jest dobrowolna, ale wymagana do podpisania „Apelu”)* <br> <br> *Zgody obowiązkowe</label>
         </div>
