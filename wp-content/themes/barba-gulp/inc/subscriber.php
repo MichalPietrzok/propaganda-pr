92e6e721b71d4491871746fb6a72a5cd47f62212
jura <yury.andreichuk@follow.vision>
2022-02-14 13:53:36 +0100

    change mail adres

--- wp-content/themes/barba-gulp/inc/subscriber.php
+++ wp-content/themes/barba-gulp/inc/subscriber.php
@@ -8,7 +8,7 @@ function add_subscriber() {
   $user_form_subject = 'Podpisanie Apelu';
   $user_name = trim($_POST["name"]);
   $user_email = trim($_POST["email"]);
-  $from_mail = 'info@prtoniepropaganda.pl';
+  $from_mail = 'joanna.dokudowicz@zfpr.pl';
   $secret_key = rand();
   $subscriber = array(
     'apel_name' => $user_name,
