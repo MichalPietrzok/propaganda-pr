bf239518c1d115ab9ad014f9e8df1f3193f23a52
jura <yury.andreichuk@follow.vision>
2022-02-15 12:26:52 +0100

    add 404

--- wp-content/themes/barba-gulp/404.php
+++ wp-content/themes/barba-gulp/404.php
@@ -16,7 +16,7 @@ get_header();
         <div class="row">
           <div class="col-12 d-flex align-items-center" style="height: 100vh">
             <div class="verification__wrap">
-              <h2 class="verification__title">Coś poszło nie tak...</h2>
+              <h2 class="verification__title">Błąd 404</h2>
               <p class="verification__information">Przejdź do <a href="<?= get_home_url()?>">Strony głównej</a></p>
               <p class="verification__information">Albo skontaktuj się z nami <a href="mailto:info@zfpr.pl">info@zfpr.pl</a></p>
             </div>
